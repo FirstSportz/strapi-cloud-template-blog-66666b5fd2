64cdcde06a4dda9e1096136da92586673fccaa03
Nikhil C <nikhil.chindarkar@ibm.com>
2024-10-02 21:01:12 +0530

    adding smtp domain and details regarding the same.

--- my-strapi-project/config/plugins.ts
+++ my-strapi-project/config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    email: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+            user: env('SMTP_USER'),
+            pass: env('SMTP_PASSWORD'),
+          },
+        },
+        settings: {
+          defaultFrom: 'aditya.mahajan@firstsportz.com',
+          defaultReplyTo: 'aditya.mahajan@firstsportz.com',
+        },
+    },
+  });
+  
